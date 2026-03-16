5697fa196bc55f09a9beb1c52837b20cb211d910
thijsrijkers <thijsrijkers@live.nl>
2026-03-16 08:16:40 +0100

    Added auto import

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,7 +13,6 @@ return {
       })
     end,
   },
-
   {
     "williamboman/mason-lspconfig.nvim",
     dependencies = { "williamboman/mason.nvim" },
@@ -27,7 +26,6 @@ return {
       })
     end,
   },
-
   {
     "neovim/nvim-lspconfig",
     dependencies = {
@@ -36,14 +34,37 @@ return {
       "hrsh7th/cmp-nvim-lsp",
     },
     config = function()
-      -- Pass cmp capabilities to LSP so autocomplete works
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+      -- Auto-import helper: runs import-related code actions on save
+      local function auto_import(bufnr)
+        local ft = vim.bo[bufnr].filetype
+
+        local filter = nil
+
+        if ft == "typescript" or ft == "typescriptreact" or ft == "javascript" or ft == "javascriptreact" then
+          filter = function(action)
+            return action.kind == "source.addMissingImports.ts"
+          end
+        elseif ft == "go" then
+          filter = function(action)
+            return action.title:match("[Oo]rganize [Ii]mports")
+          end
+        end
+
+        if filter then
+          vim.lsp.buf.code_action({
+            apply  = true,
+            filter = filter,
+            context = { only = ft == "go" and { "source.organizeImports" } or { "source.addMissingImports" } },
+          })
+        end
+      end
 
       local on_attach = function(_, bufnr)
         local map = function(keys, func, desc)
           vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
         end
-
         map("gd", vim.lsp.buf.implementation,     "Go to Implementation")
         map("gD", vim.lsp.buf.declaration,        "Go to Declaration")
         map("gT", vim.lsp.buf.type_definition,    "Go to Type Definition")
@@ -56,11 +77,16 @@ return {
         map("<leader>lf", function()
           vim.lsp.buf.format({ async = true })
         end, "Format file")
+
+        -- Auto-import on save
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          buffer   = bufnr,
+          callback = function() auto_import(bufnr) end,
+        })
       end
 
       vim.lsp.config("gopls", { on_attach = on_attach, capabilities = capabilities })
       vim.lsp.enable("gopls")
-
       vim.lsp.config("ts_ls", { on_attach = on_attach, capabilities = capabilities })
       vim.lsp.enable("ts_ls")
     end,
